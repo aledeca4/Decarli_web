c18c845b0af4434e30b37161ea10100c7fcd1c83
aledeca4 <alessandro.decarli75@gmail.com>
2026-05-03 22:55:08 +0200

    Create es06b.js

---
+++ es06b.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+
+function inverti_cifre(n) {
+  const segno = Math.sign(n);
+  const invertito = parseInt(Math.abs(n).toString().split("").reverse().join(""));
+  return segno * invertito;
+}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question("Inserisci un numero intero: ",(input) {
+  const numero = parseInt(input);
+  if (isNaN(numero)) {
+    console.log("inserisci numero valido.");
+  } else {
+    const risultato = inverti_cifre(numero);
+    console.log("Numero invertito:", risultato);
+  }
+rl.close();
+});
